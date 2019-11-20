bc63579f9f1a6a81850f5c09c06fe95aa82ccd5f
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-11-20 11:38:45 -0300

    Release no router e algumas views/movies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/new'
-  post 'movies/create'
-  get 'movies/:id' => 'movies#show'
 
-  get 'movies' => 'movies#index'
+  root to: 'movies#index'
+
+  get 'movies/index'    # recuperar todos os filmes
+  get 'movies/new'      # redireciona para o cadastro de um novo filme
+  post 'movies/create'  # evetua um novo cadastaro
+  get 'movies/:id' => 'movies#show' # recupera um filme específico
+
+  get 'movies' => 'movies#index' # alias
+  get 'movies/edit/:id' => 'movies#edit' # redireciona para página de edição
+  put 'movies/:id' => 'movies#update'     # efetua atualização
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
